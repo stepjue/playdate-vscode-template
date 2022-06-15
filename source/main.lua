cb5235c9f1178c34b634a519590db18247d85472
Stephen Jue <stepjue@gmail.com>
2022-06-15 19:33:55 -0400

    Add more imports to main.lua

--- source/main.lua
+++ source/main.lua
@@ -1,8 +1,11 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
 
 function playdate.update() 
     gfx.clear()
-    gfx.drawText("Hello World", 20, 20)
+    gfx.drawText("Hello YOYOYOYO", 20, 20)
 end
